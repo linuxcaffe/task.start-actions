ba3789a73a763fe0297b68cc976413cb4b73b21a
David J Patrick <davamundo@gmail.com>
2015-08-13 01:54:37 -0400

    psudocode

--- pirate_modify_start_actions.py
+++ pirate_modify_start_actions.py
@@ -4,7 +4,13 @@ import sys
 import os
 from tasklib.task import TaskWarrior
 
+# wiki actions
+# if task has taskwiki backlink annotation, use that
+# if task has +wiki tag and proj.any: , but no backlink, open $WIKIDIR/projname.project.wiki
+# if task has +wiki tag, but no backlink or project, open $WIKIDIR/index.wiki
+
 def hook_test(task):
         if task.active and not task.original.get('start'):
                     print("This is a working example.")
 
+# tasknote
