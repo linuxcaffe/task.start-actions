7a6079a4ec578c19087e8e7526e14df57485594c
David J Patrick <davamundo@gmail.com>
2015-08-13 04:04:55 -0400

    Update pirate_modify_start_actions.py

--- pirate_modify_start_actions.py
+++ pirate_modify_start_actions.py
@@ -3,16 +3,27 @@
 import sys
 import os
 from tasklib.task import TaskWarrior
-
-# wiki actions
-# if task has taskwiki backlink annotation, use that
-# if task has +wiki tag and proj.any: , but no backlink, open $WIKIDIR/project/projname.project.wiki
-# if task has +wiki tag, but no backlink or project, open $WIKIDIR/index.wiki
-
 def hook_test(task):
         if task.active and not task.original.get('start'):
                     print("This is a working example.")
 
+# if envar $TASKDATA exists
+#       then $TASKDIR=$TASKDATA
+# else $TASKDIR=~/.task
+
 # tasknote actions
-# if task has tasknote, open that
+# if envar $TASKNOTES exists
+#       then $NOTEDIR=$TASKNOTES
+#       else $NOTEDIR=$TASKDIR/notes
+# if task has existing tasknote, open that
 # if task has +note tag, but no tasknote, open new tasknote
+
+# wiki actions
+# if envar $TASKWIKI exists
+#       then $WIKIDIR=$TASKWIKI
+#       else $WIKIDIR=$TASKDIR/wiki
+# if task has taskwiki backlink annotation, open that
+# if task has +wiki tag and proj.any: , but no backlink, open $WIKIDIR/project/projname.project.wiki
+# if task has +wiki tag, but no backlink or project, open $WIKIDIR/index.wiki
+
+
